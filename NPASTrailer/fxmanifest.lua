91255c22e50f0d65f2a405c24d8a42ad91de9ae4
Aviation-Adam <98424168+Aviation-Adam@users.noreply.github.com>
2022-07-19 16:18:59 +0100

    Add files via upload

---
+++ NPASTrailer/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Manifest
+fx_version 'cerulean'
+games { 'gta5' }
+
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'handling.meta',
+    'carcols.meta'
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'CARCOLS_FILE' 'handling.meta'
---
+++ mpu/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+files {
+    'vehicles.meta',
+    'vehiclelayouts.meta'
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+
